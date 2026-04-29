d4c3dd9b9e356fd217ae47cf68560560a5eb6431
JosiePrescott <josieprescott88@gmail.com>
2026-04-29 08:52:23 -0600

    Fixing the layout

---
+++ 03-task-manager/starter/db/connect.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+const connectionString =
+'mongodb+srv://josieprescott88_db_user:0rnlA0cC9GA5aa4K@cluster0.rf7plx4.mongodb.net/?appName=Cluster0'
+
+mongoose.connect()
