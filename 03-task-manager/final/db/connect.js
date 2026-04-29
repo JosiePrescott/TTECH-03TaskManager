038f9d4df8ec24efba689a280a8999bb0077c578
JosiePrescott <josieprescott88@gmail.com>
2026-04-29 09:27:04 -0600

    Adding content

--- 03-task-manager/final/db/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const connectionString =
-'mongodb+srv://josieprescott88_db_user:0rnlA0cC9GA5aa4K@cluster0.rf7plx4.mongodb.net/?appName=Cluster0'
-
-mongoose.connect()
