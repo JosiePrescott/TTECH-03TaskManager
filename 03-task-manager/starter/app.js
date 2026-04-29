d313e7426692f999ff7e5c767167a59d4ccbc6b0
JosiePrescott <josieprescott88@gmail.com>
2026-04-29 09:36:11 -0600

    Changing app.js

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotnev').congfig();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 //middleware
 app.use(express.json());
+app.use(express.static('./public'));
 
 
 //routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-});
-
 app.use('/api/v1/tasks', tasks);
 
-//app.get('/apli/v1/tasks')   - get all the tasks
-//app.post('/api/v1/tasks')   -create a new task
-//app.get('/api/v1/tasks/:id') -get single task
-//app.patch('/api/v1/tasks/:id') -update task
-//app.delete('/api/v1/tasks/:id') -delete task
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 5000;
 
-const port = 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+        console.log('Server is listening on port ${port}')
+        );
+    } catch (error) {
+        console.log(error);
+    }
 
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}...`);
-});
+};
+
+start();
